dcbcac88682a28fe24bf1a98cf2f829600bb9cc5
knvelasquez <knvelasquez35@gmail.com>
2018-10-10 21:18:45 -0300

    fix compatibility browser request

--- js/script.js
+++ js/script.js
@@ -25,53 +25,59 @@ $(function(){
 	$(".button").click(function(){
 		$button=$(this);
 		$button.val("Wait...");
-		//Sending POST with parameters.				
-		$.ajax({
-			type: "POST",
-			url: $url_base("login/"),
-			crossDomain: true,
-			data:$setParam($(".form-horizontal").serializeArray()),
-			ataType: "json",
-			xhrFields: {
-			  withCredentials: true
-			},
-			success: function($result,$msg,$obj){
-				console.log($msg + " : " + $result.key);
-				//Validate if response status is 200
-				if($obj.status===200)
-				{			
-					//Sending GET with credentials to get user details
-					$.ajax({
-						type: "GET",
-						url: $url_base("user/?format=json"),
-						contentType: "application/json",
-						crossDomain: true,
-						dataType: "json",
-						xhrFields: {
-						  withCredentials: true
-						},
-						beforeSend: function(xhr) {
-							xhr.setRequestHeader("Cookie", "session=xxxyyyzzz");
-						},
-						//dataType: "json",
-						success: function($result,$msg,$obj){
-							//TODO-Go to next page
-							$.cookie("user_pk", $result.pk);
-							window.location.href = "dashboard.php";						
-						},
-						error:function($object,$error,$message){
-							console.error($error+ " " + $object.status + " : " + $message);
-						}
-					});								
-				}				
-			},
-			fail:function($object,$error,$message) {			
-				console.error($error+ " " + $object.status + " : " + $message);
-			},
-			always:function($object,$info,$message) {
-				console.info($info+ " " + $info.status + " : " + $message);
-				$button.val("Login");			
-			}
-		});					
+		//Sending POST with parameters.
+		var $login=function($withCredentials){						
+			$.ajax({
+				type: "POST",
+				url: $url_base("login/"),
+				crossDomain: true,
+				data:$setParam($(".form-horizontal").serializeArray()),			
+				xhrFields: {
+				  withCredentials: $withCredentials
+				},		
+				headers : {
+					//'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8'
+				},			
+				beforeSend: function(xhr) {	},
+				success: function($result,$msg,$obj){
+					console.log($msg + " : " + $result.key);
+					//Validate if response status is 200
+					if($obj.status===200)
+					{			
+						//Sending GET with credentials to get user details
+						$.ajax({
+							type: "GET",
+							url: $url_base("user/?format=json"),
+							contentType: "application/json",
+							//crossDomain: true,
+							//dataType: "json",
+							xhrFields: {
+							  withCredentials: true
+							},						
+							headers : {},
+							beforeSend: function(xhr) {},						
+							success: function($result,$msg,$obj){
+								//TODO-Go to next page
+								$.cookie("user_pk", $result.pk);
+								window.location.href = "dashboard.php";						
+							},
+							error:function($object,$error,$message){
+								console.warn($error+ " " + $object.status.toString() + " : " + $message);	
+								$login(true);								
+							}
+						});								
+					}				
+				},
+				error:function($object,$error,$message) {			
+					console.warn($error+ " " + $object.status.toString() + " : " + $message);
+					$login(true);
+				},
+				always:function($object,$info,$message) {
+					console.info($info+ " " + $info.status + " : " + $message);
+					$button.val("Login");			
+				}
+			});	
+		};
+		$login(false);						
 	})
 });
